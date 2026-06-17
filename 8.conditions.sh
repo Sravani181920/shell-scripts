86c20d9fde6059a1fc8d3a82c11cd153c30cccc2
Sravani181920 <sravs181920@gmail.com>
2026-06-17 11:18:10 +0530

    variables demo

--- 8.conditions.sh
+++ 8.conditions.sh
@@ -3,7 +3,7 @@
 NUMBER=$1
 if [$NUMBER -gt 100]
 then
-    echo "The number given is greater than 100'
+    echo "The number given is greater than 100"
 else
     echo "The number given is less than 100"
 fi
