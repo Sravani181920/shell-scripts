21fb1d069b2d971e23b184fb4af35d7c84d0e631
Sravani181920 <sravs181920@gmail.com>
2026-06-17 11:08:34 +0530

    variables demo

---
+++ 7.special-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+X=$!, Y=$2, Z=$3
+echo "All variables passed are:$@"
+echo "Number of variables are: $#"
+echo "Script name is: $0"
+echo "Present working directory is: $PWD"
+echo "Home directory of current user is: $HOME"
+echo "Which user is running the script: $USER"
+echo "Process id of current script is: $$"
+sleep -s 10
+echo "Process id of last command in background is: $!"
