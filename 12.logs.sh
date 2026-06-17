243ac041f354a194b7deab4de4d9aa2afd45dfd2
Sravani181920 <sravs181920@gmail.com>
2026-06-17 14:29:58 +0530

    variables demo

--- 12.logs.sh
+++ 12.logs.sh
@@ -19,26 +19,26 @@ VALIDATE(){     #function
     fi    
 }
 
-echo "Script started executing at: $TIME_STAMP" &>>$LOG_FILE_NAME
+echo "Script started executing at: $TIME_STAMP" &>>$LOG_FILE
 if [ $USERID -ne 0 ] #sudo user id is always 0
 then
     echo "ERROR:: you must be Sudo User to execute this script"
     exit 1 #exit status 1 is for failure and 0 is for success
 fi
 
-dnf list installed mysql &>>$LOG_FILE_NAME
+dnf list installed mysql &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    dnf install mysql -y &>>$LOG_FILE_NAME
+    dnf install mysql -y &>>$LOG_FILE
     VALIDATE $? "Installing mysql" #function call
 else
     echo "mysql is already installed.."
 fi
 
-dnf list installed git &>>$LOG_FILE_NAME
+dnf list installed git &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    dnf install git -y &>>$LOG_FILE_NAME
+    dnf install git -y &>>$LOG_FILE
     VALIDATE $? "Installing git" #function call
 else
     echo -e "Git is already $Y installed $N"
