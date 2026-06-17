f8de0daac2aa15559dcbdc4a4b82fed057e7b2e3
Sravani181920 <sravs181920@gmail.com>
2026-06-17 10:44:59 +0530

    variables demo

---
+++ 3.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: hi $PERSON2, how are you doing"
+echo "$PERSON2:: Hello $PERSON1, i'm doing good. what about you?"
+echo "$PERSON1:: well, i'm doing fine too"
