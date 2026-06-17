c5f2a3d4257eac98f17850e5e9a481ab69f38258
Sravani181920 <sravs181920@gmail.com>
2026-06-17 11:10:39 +0530

    variables demo

--- 7.special-variables.sh
+++ 7.special-variables.sh
@@ -7,5 +7,5 @@ echo "Present working directory is: $PWD"
 echo "Home directory of current user is: $HOME"
 echo "Which user is running the script: $USER"
 echo "Process id of current script is: $$"
-sleep -s 10
+sleep 10
 echo "Process id of last command in background is: $!"
