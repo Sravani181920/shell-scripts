f9a75b3ea07c036237b7c2a0ced51b7ebc79fc7a
Sravani181920 <sravs181920@gmail.com>
2026-06-17 13:50:55 +0530

    variables demo

--- 10.function.sh
+++ 10.function.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+#!/bin/bash
+
 USERID=$(id -u) #check if user is sudo user or not
+
+VALIDATE(){     #function
+    if [ $1 -ne 0 ]
+    then
+        echo "$2... Failed"
+        exit 1
+    else
+        echo "$2... Success"
+    fi    
+}
 if [ $USERID -ne 0 ] #sudo user id is always 0
 then
     echo "ERROR:: you must be Sudo User to execute this script"
@@ -11,13 +23,7 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then
     dnf install mysql -y
-    if [ $? -ne 0 ]
-    then
-        echo "Installing mysql... Failed"
-        exit 1
-    else
-        echo "Installing mysql... Success"
-    fi
+    VALIDATE $? "Installing mysql" #function call
 else
     echo "mysql is already installed.."
 fi
@@ -26,13 +32,7 @@ dnf list installed git
 if [ $? -ne 0 ]
 then
     dnf install git -y
-    if [ $? -ne 0 ]
-    then
-        echo "Installing git... Failed"
-        exit 1
-    else
-        echo "Installing git... Success"
-    fi
+    VALIDATE $? "Installing git" #function call
 else
     echo "Git is already installed"
 fi
